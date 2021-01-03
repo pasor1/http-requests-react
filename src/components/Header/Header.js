06b9cda6e2ab0057c844f65ec63fcf12a360e5c2
pasor1 <pasor1@gmail.com>
2021-01-03 15:05:38 +0100

    header z-index fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const header = () => {
   return (
-    <header className="fixed w-full text-center border-b-2 shadow-md py-3 mb-5 bg-gradient-to-b from-indigo-700 to-indigo-500 text-gray-100">
+    <header className="fixed w-full text-center border-b-2 shadow-md py-3 mb-5 bg-gradient-to-b from-indigo-700 to-indigo-500 text-gray-100 z-10">
       <h1>Testing</h1>
       <p className="text-lg font-semibold">Testing http requests with React</p>
     </header>
